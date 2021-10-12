755fbc760856487372652e57d42f6658ce039978
PWZER <pwzergo@gmail.com>
2021-10-12 15:40:35 +0800

    fixssh bug

--- plugins/custom.sh
+++ plugins/custom.sh
@@ -1,5 +1,8 @@
 function fixssh() {
     ssh_agent_file=$(find /tmp/ssh-*/agent.* -user ${USER} 2>/dev/null)
+    if [ ! -z "${ssh_agent_file}" ]; then
+        ssh_agent_file=$(ls -t ${ssh_agent_file[@]} | head -n1)
+    fi
     if [ ! -z "${ssh_agent_file}" ] && [ -e "${ssh_agent_file}" ]; then
         if [ ! -e ${HOME}/.ssh/ssh_auth_sock ] || [ ! "$(realpath ${HOME}/.ssh/ssh_auth_sock)" = "${ssh_agent_file}" ]; then
             ln -sf ${ssh_agent_file} ${HOME}/.ssh/ssh_auth_sock
